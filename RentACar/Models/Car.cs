9332f734e25ef5c79dc4db10a6f84bcd9a0c0036
gjcdanijel <gajicdanijel5@gmail.com>
2024-12-14 11:48:57 +0100

    Add project files.

---
+++ RentACar/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Models
+{
+	public class Car
+	{
+		public int Id { get; set; }
+		[Required]
+		public string Model { get; set; }
+		[Required]
+		public string Make { get; set; }
+		[Required]
+		public string ManufactureYear { get; set; }
+		[Required]
+		public string Fuel { get; set; }
+		public bool isAvailable { get; set; } = true;
+	}
+}
